fa1aed6740f37093f5320a6b7b1e3e5f630375c7
rymcbr <ry.mcbr@gmail.com>
2016-04-05 18:41:49 -0400

    Added files via upload

---
+++ js/init.js
@@ -0,0 +1,11 @@
+(function ($) {
+    'use strict';
+    $(function () {
+
+        $('.button-collapse').sideNav();
+
+    }); // end of document ready
+    $(document).ready(function () {
+        $('.parallax').parallax();
+    });
+})(jQuery); // end of jQuery name space
