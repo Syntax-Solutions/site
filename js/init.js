f9323ce3a1acfb824711e9f25978e6f3a8a1fe65
rymcbr <ry.mcbr@gmail.com>
2016-04-08 13:30:41 -0400

    Update init.js

--- js/init.js
+++ js/init.js
@@ -8,4 +8,8 @@
     $(document).ready(function () {
         $('.parallax').parallax();
     });
+    $(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+        $('.modal-trigger').leanModal();
+  });
 })(jQuery); // end of jQuery name space
