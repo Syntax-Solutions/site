5b031472a1c9550b0fdbdbaefd94e06668998fe7
rymcbr <ry.mcbr@gmail.com>
2016-04-23 20:04:24 -0400

    Create loader.js

---
+++ js/loader.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function ($) {
+    'use strict';
+    $(window).load(function () {
+        setTimeout(function () {
+            $('#preloader').fadeOut('slow', function () {
+            });
+        }, 800);
+
+    });
+});
