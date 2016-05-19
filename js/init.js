0db01a0a9dddd5874f95c6912e1839fdf4060276
rymcbr <ry.mcbr@gmail.com>
2016-05-19 13:12:48 -0400

    Update init.js

--- js/init.js
+++ js/init.js
@@ -8,16 +8,8 @@
     $(document).ready(function () {
         $('.parallax').parallax();
     });
-    $(document).ready(function(){
+    $(document).ready(function () {
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
         $('.modal-trigger').leanModal();
-  });
-  $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-            $('.disappear').fadeOut();
-        } else {
-            $('.disappear').fadeIn();
-        }
     });
-});
 })(jQuery); // end of jQuery name space
