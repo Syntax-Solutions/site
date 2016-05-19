c3708fdbcbcfb927f67ec123efdfd3f167877e4b
rymcbr <ry.mcbr@gmail.com>
2016-05-19 13:08:27 -0400

    Update init.js

--- js/init.js
+++ js/init.js
@@ -11,5 +11,13 @@
     $(document).ready(function(){
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
         $('.modal-trigger').leanModal();
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.disappear').fadeOut();
+        } else {
+            $('.disappear').fadeIn();
+        }
+    });
+});
   });
 })(jQuery); // end of jQuery name space
