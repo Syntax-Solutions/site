9cf5b01446d94710706aa71fa994c978b758cdc7
joshardul <joshardul@users.noreply.github.com>
2016-06-02 21:12:42 -0400

    Update init.js

--- js/init.js
+++ js/init.js
@@ -3,13 +3,9 @@
     $(function () {
 
         $('.button-collapse').sideNav();
-
-    }); // end of document ready
-    $(document).ready(function () {
         $('.parallax').parallax();
-    });
-    $(document).ready(function () {
-    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
         $('.modal-trigger').leanModal();
+        $('.slider').slider({full_width: true});
+
     });
-})(jQuery); // end of jQuery name space
+})(jQuery);
