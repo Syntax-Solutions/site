24a2c1878e85c37b13dced91bfd67ef5bcd41ef1
rymcbr <ry.mcbr@gmail.com>
2016-06-29 22:05:44 -0400

    Delete loader.js

--- text_heavy/js/loader.js
+++
@@ -1,7 +0,0 @@
-function resizeText(multiplier) {
-    'use strict';
-    if (document.body.style.fontSize === "17px") {
-        document.body.style.fontSize = "1.0em";
-    }
-    document.body.style.fontSize = parseFloat(document.body.style.fontSize) + (multiplier * 0.2) + "em";
-}
