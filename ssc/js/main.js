951cff69fdf31a70e581d621a70089b3832e19ee
rymcbr <ry.mcbr@gmail.com>
2016-07-08 11:01:31 -0400

    Create main.js

---
+++ ssc/js/main.js
@@ -0,0 +1,11 @@
+function openNav() {
+    'use strict';
+    document.getElementById("mySidenav").style.width = "200px";
+    document.getElementById("main").style.marginLeft = "200px";
+}
+
+function closeNav() {
+    'use strict';
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
