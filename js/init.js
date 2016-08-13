935dfdbb5b908b0a2934334fa28cf5aca3067cee
rymcbr <ry.mcbr@gmail.com>
2016-08-13 13:39:53 -0700

    Add files via upload

--- js/init.js
+++ js/init.js
@@ -1,11 +1,12 @@
 (function ($) {
     'use strict';
     $(function () {
-
         $('.button-collapse').sideNav();
         $('.materialboxed').materialbox();
         $('.modal-trigger').leanModal();
-        $('.slider').slider({full_width: true});
-        
+        $('.slider').slider({
+            full_width: true
+        });
+        $('.parallax').parallax();
     });
 })(jQuery);
